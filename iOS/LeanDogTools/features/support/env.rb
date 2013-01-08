fbe461b7ef671d5ae5b2b424e7a081ea13d97e0f
Levi Wilson <levi@leviwilson.com>
2013-01-08 13:31:54 -0500

    setup environment for launching the app

--- iOS/LeanDogTools/features/support/env.rb
+++ iOS/LeanDogTools/features/support/env.rb
@@ -1,5 +1,7 @@
-
 require 'rspec-expectations'
+require 'frank-cucumber'
+require 'furter'
 
+Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 
-
+World(Furter::Navigation)
---
+++ iOS/LeanDogTools/features/support/hooks.rb
@@ -0,0 +1,18 @@
+Before do
+  launch_app
+end
+
+After do
+  simulator_reset
+end
+
+def simulator_reset
+    %x{osascript<<APPLESCRIPT
+        tell application "System Events"
+          click menu item 5 of menu "iOS Simulator" of menu bar of process "iPhone Simulator"
+            tell window 1 of process "iPhone Simulator"
+              click button "Reset"
+            end tell
+        end tell
+    APPLESCRIPT}
+end
